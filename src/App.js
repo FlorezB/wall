397e6a35a784ffe57e82fda2ae1d3a5aebb27201
Caroline <alyseflorez@hotmail.fr>
2022-07-12 12:20:42 +0200

    exo fini

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react'
 import users from './users.json'
 import Card from './components/Card'
 
+
+
 class App extends React.Component {
   render() {
     return (
-      // hello
+      <section>
+        <h1>The wall</h1>
+        <article>
+        {users.map(user => (
+          <Card picture={user.picture} name={user.name} about={user.about}/>
+        ))}
+        </article>
+      </section>
     )
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,16 @@ import React from 'react'
 class Card extends React.Component {
   render() {
     return (
-      // hello
+
+      <article className="box col-sm-3 col-6">
+        <div className="profil">
+          <img src={this.props.picture} alt="gris 128x128"/>
+          <h2> {this.props.name}</h2>
+          <div>
+            <p>{this.props.about}</p>
+          </div>
+        </div>
+      </article>
     )
   }
 }
